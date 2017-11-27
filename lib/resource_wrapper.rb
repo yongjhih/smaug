c27e00a0bbba82158f0a0a3905eec66873b8bead
Bradley Priest <bradleypriest@gmail.com>
2017-11-27 10:24:05 +0800

    Remove resources because they’re broken and I can’t work out why

--- lib/resource_wrapper.rb
+++ lib/resource_wrapper.rb
@@ -5,7 +5,7 @@ end
 class ResourceWrapper < OpenStruct
   def self.wrap(resource_name)
     raw = I18n.backend.send(:translations)[I18n.locale][resource_name.to_sym]
-    ResourceWrapper.new(raw)
+    ResourceWrapper.new(raw).tap { |wrapper| wrapper._resource_name = resource_name }
   end
 
   def available?
